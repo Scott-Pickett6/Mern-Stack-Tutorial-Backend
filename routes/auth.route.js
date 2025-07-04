f2f1370dcbd5587ea1bbe7054ccba8107321180c
Scott Pickett <sc839110@dal.ca>
2025-07-04 16:38:29 -0300

    tutorial6

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { login, register } from '../controllers/auth.controller.js';
+import { validateLogin, validateRegister } from '../validators/authValidators.js';
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateRegister, register);
+router.post('/login', validateLogin, login);
 
 export default router;
