382bd2059899cbbc31110ef7cda2a934f30bf7b0
Scott Pickett <sc839110@dal.ca>
2025-07-11 16:07:09 -0300

    fix

--- models/user.model.js
+++ models/user.model.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         enum: ["customer", "admin"],
+        default: "customer"
     }
 }, {
     timestamps: true
